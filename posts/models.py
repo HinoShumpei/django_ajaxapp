1be8ccc33c9fb3998f075e49f8ad069847885b43
Asuka Toyoshima <asuka.toyoshima@di-v.co.jp>
2024-08-13 15:40:44 +0900

    最新のFirstAppに合わせ修正

--- posts/models.py
+++ posts/models.py
@@ -2,5 +2,8 @@ from django.db import models
 from django.utils import timezone 
 
 class Post(models.Model):
-  content = models.TextField(blank=True)
-  created_at = models.DateTimeField(verbose_name='投稿日時', default=timezone.now)
+  class Meta:
+    db_table = 'posts'
+
+  content = models.CharField(blank=True)
+  created_at = models.DateTimeField(default=timezone.now)
