3ad6574a844de2763d59567e645d9af3a88909bd
HinoShumpei <shumpei.hino@di-v.co.jp>
2024-08-16 14:57:28 +0900

    コミットする

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.views import View
 from posts.models import Post
 from .forms import PostForm
@@ -12,9 +13,15 @@ class IndexView(View):
   def post(self, request, *args, **kwargs):
     form = PostForm(request.POST)
     if form.is_valid():
-      form.save()
-    return redirect('posts:index')
-    
+      post = form.save()
+      return JsonResponse({
+        'content': post.content,
+        'created_at': post.created_at
+      })
+    else :
+      return JsonResponse({
+            'errors': form.errors
+        }, status=400)
 # class CreateView(View):
 #   def get(self, request, *args, **kwargs):
 #     form = PostForm()
